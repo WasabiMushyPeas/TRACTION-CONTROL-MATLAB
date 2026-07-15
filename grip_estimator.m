f8e66cf4e08944a35deaf611440228f5f5b01140
Jackson k McDowell <Jackson.McDowell@gmail.com>
2026-07-15 08:15:42 -0700

    grip estimator update

--- grip_estimator.m
+++ grip_estimator.m
@@ -1,6 +1,8 @@
 function maxDriveTorque = grip_estimator(normalLoad, params)
     normalLoad = max(normalLoad(:).', 0);
+    
     availableFriction = max((params.tirePeakMuBase - ...
     params.tirePeakMuLoadSensitivity .* normalLoad) .* params.globalGripScale, 0.1);
+
     maxDriveTorque = availableFriction .* normalLoad .* params.wheelRadius ./ (params.gearRatio * params.drivetrainEfficiency);
 end
